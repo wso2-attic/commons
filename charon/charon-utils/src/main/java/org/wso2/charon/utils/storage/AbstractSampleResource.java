31292a6377d3f1a004984e1d178c1dc2f588270e
hasini <hasini@a5903396-d722-0410-b921-86c7d4935375>
2012-03-11 21:30:36 +0000

    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@122370 a5903396-d722-0410-b921-86c7d4935375

---
+++ charon/charon-utils/src/main/java/org/wso2/charon/utils/storage/AbstractSampleResource.java
@@ -0,0 +1,60 @@
+/*
+*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.charon.utils.storage;
+
+import java.util.Date;
+
+public abstract class AbstractSampleResource {
+    private Date createdDate;
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    private Date lastModified;
+    private String location;
+    private String version;
+}
