1d06272ef26bd01222bf75aa22f7358f962cfa6c
charitha <charitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-31 14:18:21 +0000

    updated the script
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@135963 a5903396-d722-0410-b921-86c7d4935375

--- qa/qa-artifacts/apimanager/data-populator-scripts/client.sh
+++ qa/qa-artifacts/apimanager/data-populator-scripts/client.sh
@@ -3,7 +3,7 @@ curl -X POST -c cookies $SERVER/publisher/site/blocks/user/login/ajax/login.jag 
 for ((i=10; i <= 1000; i++))
 do
    echo "********Create API $i********"
-   curl -X POST -b cookies $SERVER/publisher/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=C_API$i&version=1.0.$i&description=Customer JAX-RS&endpoint=http://10.100.3.238:9843/jaxrs_basic/services/customers/customerservice&wadl=http://10.100.3.238:9843/jaxrs_basic/services/customers?_wadl&tags=cust,jax-rs,open,social&tier=Unlimited&thumbUrl=https://lh6.ggpht.com/RNc8dD2hXG_rWGzlj09ZwAe1sXVvLWkeYT3ePx7zePCy4ZVV2XMGIxAzup4cKM85NFtL=w124&context=/customerservice$i&tiersCollection=Gold&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
+   curl -X POST -b cookies $SERVER/publisher/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=C_API$i&version=1.0.$i&description=Customer JAX-RS&endpoint=http://10.100.3.238:9843/jaxrs_basic/services/customers/customerservice&wadl=http://10.100.3.238:9843/jaxrs_basic/services/customers?_wadl&tags=cust$i,jax-rs,open,social&tier=Unlimited&thumbUrl=https://lh6.ggpht.com/RNc8dD2hXG_rWGzlj09ZwAe1sXVvLWkeYT3ePx7zePCy4ZVV2XMGIxAzup4cKM85NFtL=w124&context=/customerservice$i&tiersCollection=Gold&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
 
    echo "********Publish API $i********"
 curl -X POST -b cookies $SERVER/publisher/site/blocks/life-cycles/ajax/life-cycles.jag -d "name=C_API$i&version=1.0.$i&provider=user1-creator&status=PUBLISHED&publishToGateway=true&action=updateStatus"
