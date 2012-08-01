30ffa5088fd8073c653a3fb0424a5943b048dc53
evanthika <evanthika@a5903396-d722-0410-b921-86c7d4935375>
2012-08-01 13:36:42 +0000

    This script logs 1 -100 users to the API store
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@136029 a5903396-d722-0410-b921-86c7d4935375

---
+++ qa/qa-artifacts/apimanager/data-populator-scripts/addUser.sh
@@ -0,0 +1,8 @@
+SERVER=http://localhost:9765
+
+for ((i=1; i <= 100; i++))
+do
+   echo "********Logging into Store $i********"
+curl -X POST  $SERVER/store/site/blocks/user/sign-up/ajax/user-add.jag -d"action=addUser&username=subscriber$i&password=evanthika123#"
+#curl -X POST  $SERVER/store/site/pages/sign-up.jag -d 'action=addUser&username=subscriber$i&password=evanthika123#'
+done
