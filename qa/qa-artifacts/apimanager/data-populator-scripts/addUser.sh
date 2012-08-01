0e51cb5dd16bc7baef8319e40aa81be70b1746ad
evanthika <evanthika@a5903396-d722-0410-b921-86c7d4935375>
2012-08-01 13:37:25 +0000

    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@136030 a5903396-d722-0410-b921-86c7d4935375

--- qa/qa-artifacts/apimanager/data-populator-scripts/addUser.sh
+++ qa/qa-artifacts/apimanager/data-populator-scripts/addUser.sh
@@ -2,7 +2,7 @@ SERVER=http://localhost:9765
 
 for ((i=1; i <= 100; i++))
 do
-   echo "********Logging into Store $i********"
+   echo "********Adding subscribers$i********"
 curl -X POST  $SERVER/store/site/blocks/user/sign-up/ajax/user-add.jag -d"action=addUser&username=subscriber$i&password=evanthika123#"
 #curl -X POST  $SERVER/store/site/pages/sign-up.jag -d 'action=addUser&username=subscriber$i&password=evanthika123#'
 done
