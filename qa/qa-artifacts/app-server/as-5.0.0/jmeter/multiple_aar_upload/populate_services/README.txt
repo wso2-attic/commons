ee67af0527da3e671039882078fb1226a9eae185
evanthika@wso2.com <evanthika@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-03-16 11:13:29 +0000

    This is a script which can create multiple axis2services
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@168119 a5903396-d722-0410-b921-86c7d4935375

---
+++ qa/qa-artifacts/app-server/as-5.0.0/jmeter/multiple_aar_upload/populate_services/README.txt
@@ -0,0 +1,3 @@
+How can I create multiple axis2services?
+========================================
+To create 100 axis2 services, run gen.sh
--- qa/qa-artifacts/app-server/as-5.0.0/jmeter/populate_services/axis2/gen.sh
+++ qa/qa-artifacts/app-server/as-5.0.0/jmeter/populate_services/axis2/gen.sh
@@ -2,13 +2,13 @@
 
 cd /opt/svn_repos/qa_artifacts/qa-artifacts/app-server/as-5.0.0/jmeter/populate_services/axis2
 i=1
-while test $i != 11
+while test $i != 101
 do
-sed 's/Axis2SampleService/Axis2SampleService_'$i'/g' service.xml > ./service/META-INF/services.xml
+sed 's/Axis2SampleService/Axis2SampleService_t1_'$i'/g' service.xml > ./service/META-INF/services.xml
 cd ./service
-jar -cvf Axis2SampleService_${i}.aar *
+jar -cvf Axis2SampleService_t1_${i}.aar *
 #mkdir ../bulk
-mv Axis2SampleService_${i}.aar /home/evanthika/WSO2/CARBON/CARBON4/AS/axis2services
+mv Axis2SampleService_t1_${i}.aar ../bulk
 i=`expr $i + 1`
 cd ../
 done
