7c5d22a1e5734a8ae6559ef9065fd1c2b382bc44
evanthika@wso2.com <evanthika@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-03-17 15:29:48 +0000

    Jmeter script to apply UT for multiple services
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@168166 a5903396-d722-0410-b921-86c7d4935375

--- qa/qa-artifacts/app-server/as-5.0.0/jmeter/multiple_aar_upload/populate_services/gen.sh
+++ qa/qa-artifacts/app-server/as-5.0.0/jmeter/multiple_aar_upload/populate_services/gen.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd /opt/svn_repos/qa_artifacts/qa-artifacts/app-server/as-5.0.0/jmeter/populate_services/axis2
+cd /opt/svn_repos/qa_artifacts/qa-artifacts/app-server/as-5.0.0/jmeter/multiple_aar_upload/populate_services
 i=1
 while test $i != 101
 do
-sed 's/Axis2SampleService/Axis2SampleService_t1_'$i'/g' service.xml > ./service/META-INF/services.xml
+sed 's/Axis2SampleService/Axis2SampleService_t2_'$i'/g' service.xml > ./service/META-INF/services.xml
 cd ./service
-jar -cvf Axis2SampleService_t1_${i}.aar *
+jar -cvf Axis2SampleService_t2_${i}.aar *
 #mkdir ../bulk
-mv Axis2SampleService_t1_${i}.aar ../bulk
+mv Axis2SampleService_t2_${i}.aar ../bulk2
 i=`expr $i + 1`
 cd ../
 done
