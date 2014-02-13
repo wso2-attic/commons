0ea265340d18c7dd75222860c74c35ae65cdef7b
evanthika@wso2.com <evanthika@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2014-02-13 11:18:56 +0000

    Mind-map on SAML2 SSO (draft)
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@196806 a5903396-d722-0410-b921-86c7d4935375

---
+++ qa/qa-artifacts/Mind_Maps/IS/Single_Sign-On.mm
@@ -0,0 +1,15 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1392192698298" ID="Freemind_Link_923384270" MODIFIED="1392197554240" TEXT="SAML2 based Single Sign-On">
+<node CREATED="1392192957728" ID="_" MODIFIED="1392193696694" POSITION="right" TEXT="As a consumer">
+<node CREATED="1392206969484" ID="Freemind_Link_1953130601" MODIFIED="1392206977640" TEXT="With Shibboleth SAML2 IDP"/>
+</node>
+<node CREATED="1392193707396" ID="Freemind_Link_1582613516" MODIFIED="1392210998724" POSITION="left" TEXT="As an identity provider">
+<node CREATED="1392196470118" ID="Freemind_Link_1351194519" MODIFIED="1392211018322" TEXT="SAML2 Web Browser Single Sign-On profile">
+<node CREATED="1392211259190" ID="Freemind_Link_1906887253" MODIFIED="1392211282476" TEXT="Single Identity providers"/>
+<node CREATED="1392211285357" ID="Freemind_Link_847720508" MODIFIED="1392211294166" TEXT="Multiple Identity providers"/>
+</node>
+<node CREATED="1392197573249" ID="Freemind_Link_1981814659" MODIFIED="1392197577874" TEXT="Single Logout Profile"/>
+</node>
+</node>
+</map>
