331052acf1a9505a8c09db7ed4a459a8277bb5af
pavithra@wso2.com <pavithra@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2014-02-18 09:07:28 +0000

    Updating the modified mind maps done by Ushani Balasooriya
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@196957 a5903396-d722-0410-b921-86c7d4935375

--- qa/qa-artifacts/Mind_Maps/IS/IS_Architecture.mm
+++ qa/qa-artifacts/Mind_Maps/IS/IS_Architecture.mm
@@ -3,56 +3,26 @@
 <node BACKGROUND_COLOR="#ffffff" CREATED="1391666537981" ID="Freemind_Link_1595269407" MODIFIED="1391673371855" TEXT="Identity Server Architecture">
 <edge STYLE="linear"/>
 <font BOLD="true" NAME="SansSerif" SIZE="16"/>
-<node CREATED="1391667036494" ID="_" MODIFIED="1391673311701" POSITION="right" TEXT="System and User Identity Management">
+<node CREATED="1391667036494" ID="_" MODIFIED="1392712280605" POSITION="right" TEXT="System and Identity Management">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391667288606" ID="Freemind_Link_642178220" MODIFIED="1391667291923" TEXT="User Stores">
-<node CREATED="1391667130832" ID="Freemind_Link_1082543796" MODIFIED="1391667151752" TEXT="Built In LDAP"/>
-<node CREATED="1391667170631" ID="Freemind_Link_1070697970" MODIFIED="1391667174204" TEXT="JDBC connection"/>
-<node CREATED="1391667155833" ID="Freemind_Link_120010034" MODIFIED="1391667165713" TEXT="Micr AD"/>
-<node CREATED="1391667137928" ID="Freemind_Link_598419819" MODIFIED="1391667146067" TEXT="External LDAP"/>
+<node CREATED="1392712203262" ID="Freemind_Link_1545370941" MODIFIED="1392712213810" TEXT="User Account verification method"/>
+<node CREATED="1392712219573" ID="Freemind_Link_453585834" MODIFIED="1392712220518" TEXT="Password recovery methods"/>
+<node CREATED="1392712224828" ID="Freemind_Link_1902543557" MODIFIED="1392712225515" TEXT="User account recovery methods"/>
+<node CREATED="1392712229677" ID="Freemind_Link_1790077638" MODIFIED="1392712230349" TEXT="Account locking / unlocking"/>
+<node CREATED="1392712236549" ID="Freemind_Link_410590464" MODIFIED="1392712240305" TEXT="Password management">
+<node CREATED="1392712245445" ID="Freemind_Link_874785087" MODIFIED="1392712246199" TEXT=" Password policy"/>
+<node CREATED="1392712250469" ID="Freemind_Link_1982591240" MODIFIED="1392712251318" TEXT="Onetime passwords"/>
 </node>
 </node>
-<node CREATED="1391667425550" ID="Freemind_Link_138584746" MODIFIED="1391673319563" POSITION="left" TEXT="Entitlement management">
+<node CREATED="1391667425550" ID="Freemind_Link_138584746" MODIFIED="1391673319563" POSITION="right" TEXT="Entitlement management">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1391667435686" ID="Freemind_Link_1995606325" MODIFIED="1391667441247" TEXT="REST"/>
 <node CREATED="1391667443120" ID="Freemind_Link_1076964287" MODIFIED="1391667444693" TEXT="SOAP"/>
-<node CREATED="1391667468584" ID="Freemind_Link_455485563" MODIFIED="1391667522918" TEXT="Attribute and Claim based access control">
-<node CREATED="1391667474975" ID="Freemind_Link_294103792" MODIFIED="1391667478330" TEXT="XACML"/>
-<node CREATED="1391667514839" ID="Freemind_Link_220559117" MODIFIED="1391667527170" TEXT="WS-Trust"/>
-<node CREATED="1391667538373" ID="Freemind_Link_1604568132" MODIFIED="1391667541105" TEXT="Open ID"/>
-<node CREATED="1391667543399" ID="Freemind_Link_1437003402" MODIFIED="1391667547259" TEXT="Open ID Connect"/>
-<node CREATED="1391667552031" ID="Freemind_Link_1663837" MODIFIED="1391667554749" TEXT="Claim Management"/>
-</node>
-<node CREATED="1391674419230" ID="Freemind_Link_152360351" MODIFIED="1391674426092" TEXT="Managing functions">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391674431950" ID="Freemind_Link_1473201004" MODIFIED="1391674441372" TEXT="Creating an XACML Policy"/>
-<node CREATED="1391674448278" ID="Freemind_Link_1128533970" MODIFIED="1391674449191" TEXT="Editing an XACML Policy"/>
-<node CREATED="1391674455086" ID="Freemind_Link_965357891" MODIFIED="1391674456357" TEXT="Activating and Deactivating an XACML Policy"/>
-<node CREATED="1391674460694" ID="Freemind_Link_847107319" MODIFIED="1391674461419" TEXT="Importing an XACML Policy"/>
-<node CREATED="1391674465966" ID="Freemind_Link_1422310653" MODIFIED="1391674466646" TEXT="Evaluating an XACML Policy"/>
-<node CREATED="1391674471182" ID="Freemind_Link_1707848622" MODIFIED="1391674472434" TEXT="Creating basic XACML requests for Evaluation"/>
-<node CREATED="1391674477222" ID="Freemind_Link_1231480172" MODIFIED="1391674477974" TEXT="Clearing a Cache"/>
-<node CREATED="1391674482462" ID="Freemind_Link_382983231" MODIFIED="1391674483083" TEXT="Identity Server as an XACML Engine"/>
-<node CREATED="1391674492750" ID="Freemind_Link_1917698565" MODIFIED="1391674493469" TEXT="Adding XACML Fine-grained Authorization for Proxy Services in ESB"/>
-<node CREATED="1391674547685" ID="Freemind_Link_1402927980" MODIFIED="1391674548844" TEXT=" Improving XACML PDP Performance with Caching Techniques "/>
-<node CREATED="1391674554293" ID="Freemind_Link_544260713" MODIFIED="1391674555125" TEXT=" Integrating WSO2 Identity Server with Liferay "/>
-</node>
-</node>
-<node CREATED="1391667571286" ID="Freemind_Link_729752100" MODIFIED="1391673316352" POSITION="right" TEXT="XACML Support">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391668223840" ID="Freemind_Link_330332261" MODIFIED="1391672275430" TEXT="high performance network protocol "/>
+<node CREATED="1391667571286" ID="Freemind_Link_729752100" MODIFIED="1392711933436" TEXT="XACML Support">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1391672232222" ID="Freemind_Link_1247357955" MODIFIED="1391672252747" TEXT="supports policy distribution to various Policy Decision Points (PDPs)"/>
 <node CREATED="1391672257054" ID="Freemind_Link_135054063" MODIFIED="1391672258838" TEXT="supports multiple Policy Information Point (PIP) "/>
-<node CREATED="1391672300502" ID="Freemind_Link_626554760" MODIFIED="1391672487875" TEXT="XACML 2.0 and XACML 3.0 (Improvements in 3.0)">
-<node CREATED="1391672350002" ID="Freemind_Link_1337047990" MODIFIED="1391672351796" TEXT="Custom attribute categories"/>
-<node CREATED="1391672362187" ID="Freemind_Link_1874314792" MODIFIED="1391672363820" TEXT="Improvements in Obligation"/>
-<node CREATED="1391672444692" ID="Freemind_Link_176311903" MODIFIED="1391672449827" TEXT="Improvements in targets"/>
-<node CREATED="1391672463937" ID="Freemind_Link_1136200783" MODIFIED="1391672466026" TEXT="Introducing Advice"/>
-<node CREATED="1391672499176" ID="Freemind_Link_1523144342" MODIFIED="1391672500089" TEXT="More Functions and Algorithms"/>
-<node CREATED="1391672506920" ID="Freemind_Link_1887105466" MODIFIED="1391672507753" TEXT="Improvements in XPath"/>
-<node CREATED="1391672513335" ID="Freemind_Link_1745539581" MODIFIED="1391672514437" TEXT="Improvement in XACML Request and Response"/>
-<node CREATED="1391672519959" ID="Freemind_Link_1805936273" MODIFIED="1391672521533" TEXT="Multiple Decision Profile"/>
-<node CREATED="1391672544592" ID="Freemind_Link_835464523" MODIFIED="1391672546289" TEXT="Administrative Delegation Profile"/>
+<node CREATED="1391672300502" ID="Freemind_Link_626554760" MODIFIED="1391672487875" TEXT="XACML 2.0 and XACML 3.0 (Improvements in 3.0)"/>
 </node>
 </node>
 <node CREATED="1391672631343" ID="Freemind_Link_1590569020" MODIFIED="1391673318832" POSITION="left" TEXT="Management and Monitoring">
@@ -68,7 +38,6 @@
 </node>
 <node CREATED="1391672853005" ID="Freemind_Link_1125916718" MODIFIED="1391673316979" POSITION="right" TEXT="Key Stores management">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391672885541" ID="Freemind_Link_109597520" MODIFIED="1391672912231" TEXT="A key pair with a certificate signed by a trusted Certification Authority (CA)"/>
 <node CREATED="1391672932412" ID="Freemind_Link_1488714744" MODIFIED="1391672944071" TEXT="Provides UI and an API to manage keystores ">
 <node CREATED="1391673025755" ID="Freemind_Link_1168978402" MODIFIED="1391673030248" TEXT="Add key store"/>
 <node CREATED="1391673032171" ID="Freemind_Link_1146420640" MODIFIED="1391673035855" TEXT="Delete key store"/>
@@ -79,90 +48,52 @@
 </node>
 <node CREATED="1391673008651" ID="Freemind_Link_143595607" MODIFIED="1391673012362" TEXT="Multiple key stores"/>
 </node>
-<node CREATED="1391673183882" FOLDED="true" ID="Freemind_Link_1370162609" MODIFIED="1391674219425" POSITION="left" TEXT="XKMS">
+<node CREATED="1391673183882" ID="Freemind_Link_1370162609" MODIFIED="1391674219425" POSITION="left" TEXT="XKMS">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="button_cancel"/>
-<node CREATED="1391673193122" ID="Freemind_Link_1115518429" MODIFIED="1391673194131" TEXT="Key Management Service Specification (XKMS) "/>
-<node CREATED="1391673845446" ID="Freemind_Link_570112618" MODIFIED="1391673846324" TEXT="expose a Java key store ">
-<node CREATED="1391673855675" ID="Freemind_Link_983447023" MODIFIED="1391673856856" TEXT="Registration of a Key Pair"/>
-<node CREATED="1391673879366" ID="Freemind_Link_950526989" MODIFIED="1391673951133" TEXT="Reissue of a certificate with new validity interval"/>
-<node CREATED="1391673972149" ID="Freemind_Link_397287505" MODIFIED="1391673973234" TEXT="Recovering a Key Pair "/>
-<node CREATED="1391673980922" ID="Freemind_Link_1824949601" MODIFIED="1391673981892" TEXT="Locating a Public Key"/>
-<node CREATED="1391673988154" ID="Freemind_Link_1749903771" MODIFIED="1391673988941" TEXT="Validating a Public Key "/>
 </node>
-</node>
-<node CREATED="1391674254256" ID="Freemind_Link_16327597" MODIFIED="1391674257121" POSITION="right" TEXT="Single Sign-On">
+<node CREATED="1391676876903" ID="Freemind_Link_1830046335" MODIFIED="1392712291225" POSITION="right" TEXT="User Stores/ User management">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391676638663" ID="Freemind_Link_376935182" MODIFIED="1391676639625" TEXT=" secure and convenient authentication mechanism"/>
-<node CREATED="1391676679159" ID="Freemind_Link_1620677672" MODIFIED="1391676681609" TEXT="login to multiple web applications once they are authenticated at a one place using a single credential"/>
-<node CREATED="1391674868641" ID="Freemind_Link_1859213535" MODIFIED="1391674932849" TEXT="complies with the SAML2 Web Browser SSO profile and Single Logout Profile"/>
-<node CREATED="1391674901194" ID="Freemind_Link_896789499" MODIFIED="1391674923570" TEXT="Add service providers from the &quot;SAML 2.0-based Single Sign-On&quot; page"/>
-<node CREATED="1391674938777" ID="Freemind_Link_4099920" MODIFIED="1391674939627" TEXT="Deleting a Service Provider"/>
-<node CREATED="1391675026368" ID="Freemind_Link_233118439" MODIFIED="1391675027073" TEXT="Working With SAML2 Based SSO"/>
-<node CREATED="1391675048360" ID="Freemind_Link_1821874985" MODIFIED="1391675049237" TEXT="Configuring Single Sign-On Across Different Carbon Servers"/>
-<node CREATED="1391675126543" ID="Freemind_Link_184663547" MODIFIED="1391675127656" TEXT="Configuring SSO Session Timeout"/>
-<node CREATED="1391675134007" ID="Freemind_Link_1068979220" MODIFIED="1391675134809" TEXT="SAML2 IdP with SimpleSAMLphp Service Provider"/>
-<node CREATED="1391675141183" ID="Freemind_Link_298161146" MODIFIED="1391675142943" TEXT="Enabling SSO for WSO2 Servers"/>
-<node CREATED="1391675149879" ID="Freemind_Link_1896191588" MODIFIED="1391675150524" TEXT="Single Sign-On Across Web Applications and Web Services"/>
-<node CREATED="1391675154935" ID="Freemind_Link_589915403" MODIFIED="1391675155911" TEXT="Enabling SSO using OpenSSO as the IDP"/>
-</node>
-<node CREATED="1391676721272" ID="Freemind_Link_1870026612" MODIFIED="1391676730617" POSITION="left" TEXT="OAuth">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391676760456" ID="Freemind_Link_1492384376" MODIFIED="1391676773787" TEXT="Enabling OAuth support"/>
-<node CREATED="1391676778248" ID="Freemind_Link_449858013" MODIFIED="1391676787107" TEXT="Deleting OAuth support"/>
-<node CREATED="1391676799752" ID="Freemind_Link_1620464034" MODIFIED="1391676800573" TEXT="2-legged OAuth with OAuth 1.0 and 2.0"/>
-<node CREATED="1391676805384" ID="Freemind_Link_648543271" MODIFIED="1391676806259" TEXT="2-legged OAuth for Securing a RESTful Service"/>
-<node CREATED="1391676810696" ID="Freemind_Link_318589535" MODIFIED="1391676812088" TEXT="SAML2 Bearer Assertion Profile for OAuth 2.0"/>
-<node CREATED="1391676818142" ID="Freemind_Link_353991631" MODIFIED="1391676818841" TEXT="OAuth Token Revocation with WSO2 Identity Server"/>
-<node CREATED="1391676823408" ID="Freemind_Link_86266254" MODIFIED="1391676824036" TEXT="OAuth 2.0 Playground with WSO2 Identity Server"/>
-<node CREATED="1391676829038" ID="Freemind_Link_76453150" MODIFIED="1391676829698" TEXT="OAuth 2.0 Grant Types with WSO2 API Manager Authorization Code"/>
-<node CREATED="1391676834215" ID="Freemind_Link_1793998919" MODIFIED="1391676834884" TEXT="OAuth 2.0 Grant Types with WSO2 API Manager Implicit"/>
-<node CREATED="1391676840351" ID="Freemind_Link_1841561299" MODIFIED="1391676841127" TEXT="Managing OpenID Connect"/>
-</node>
-<node CREATED="1391676876903" ID="Freemind_Link_1830046335" MODIFIED="1391676879412" POSITION="right" TEXT="User Stores">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1391667137928" ID="Freemind_Link_598419819" MODIFIED="1391667146067" TEXT="External LDAP"/>
+<node CREATED="1391667155833" ID="Freemind_Link_120010034" MODIFIED="1391667165713" TEXT="Micr AD"/>
+<node CREATED="1391667170631" ID="Freemind_Link_1070697970" MODIFIED="1391667174204" TEXT="JDBC connection"/>
+<node CREATED="1391667130832" ID="Freemind_Link_1082543796" MODIFIED="1391667151752" TEXT="Built In LDAP"/>
 </node>
 <node CREATED="1391676884039" ID="Freemind_Link_593689686" MODIFIED="1391676888878" POSITION="left" TEXT="XMPP">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391677467729" ID="Freemind_Link_583218151" MODIFIED="1391677468522" TEXT="multi-factor authentication">
-<node CREATED="1391677514825" ID="Freemind_Link_72388008" MODIFIED="1391677534313" TEXT="Something you know + something you have E.g., ATM card + PIN No"/>
-</node>
-</node>
-<node CREATED="1391676894255" ID="Freemind_Link_885406700" MODIFIED="1391676896666" POSITION="right" TEXT="SCIM">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391676954646" ID="Freemind_Link_519326290" MODIFIED="1391676955447" TEXT="System for Cross-domain Identity Management (SCIM) "/>
-<node CREATED="1391678691477" ID="Freemind_Link_106511011" MODIFIED="1391678693435" TEXT=" open standard which defines a comprehensive REST API "/>
-<node CREATED="1391678734701" ID="Freemind_Link_749301720" MODIFIED="1391678750165" TEXT="    Implementing SCIM with Charon   "/>
-<node CREATED="1391678726978" ID="Freemind_Link_818224310" MODIFIED="1391678765494" TEXT=" WSO2 Identity Server as a SCIM Service Provider "/>
-<node CREATED="1391678774046" ID="Freemind_Link_858787084" MODIFIED="1391678787158" TEXT="   Registering SCIM Providers   "/>
-<node CREATED="1391678793218" ID="Freemind_Link_574765531" MODIFIED="1391678806676" TEXT="  Viewing SCIM Provider Configurations "/>
-<node CREATED="1391678795624" ID="Freemind_Link_623642061" MODIFIED="1391678823371" TEXT="Updating SCIM Provider Configurations"/>
-<node CREATED="1391678827796" ID="Freemind_Link_740759224" MODIFIED="1391678843041" TEXT="Deleting the SCIM Provider  "/>
-<node CREATED="1391678856629" ID="Freemind_Link_1940748880" MODIFIED="1391678869713" TEXT="   Identity Provisioning from On-premise to Cloud "/>
-<node CREATED="1391678850161" ID="Freemind_Link_751423558" MODIFIED="1391678944596" TEXT="  SCIM User Provisioning With IS Having Active Directory User Store  "/>
-<node CREATED="1391678954525" ID="Freemind_Link_1869638998" MODIFIED="1391678971367" TEXT="   Identity Synchronization Across Multiple Nodes  "/>
-<node CREATED="1391678965275" ID="Freemind_Link_8839458" MODIFIED="1391678990307" TEXT="Extensible SCIM User Schemas With WSO2 Identity Server "/>
-<node CREATED="1391678993618" ID="Freemind_Link_665065085" MODIFIED="1391678995133" TEXT="OAuth Bearer Token-based Authentication for SCIM Endpoints"/>
 </node>
 <node CREATED="1391676905343" ID="Freemind_Link_143711765" MODIFIED="1391676907895" POSITION="left" TEXT="OpenID">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391676985430" ID="Freemind_Link_1887809559" MODIFIED="1391678554160" TEXT=" Control how much of that information is shared with the websites you visit"/>
-<node CREATED="1391677609432" ID="Freemind_Link_1115275068" MODIFIED="1391677610380" TEXT="Getting an OpenID"/>
-<node CREATED="1391677615720" ID="Freemind_Link_910173522" MODIFIED="1391677616400" TEXT="Enabling XMPP-Based Authentication for OpenIDs"/>
 </node>
 <node CREATED="1391676914135" ID="Freemind_Link_1841462278" MODIFIED="1391676916663" POSITION="right" TEXT="STS">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1391677002118" ID="Freemind_Link_1766722620" MODIFIED="1391677002844" TEXT="Security Token Service"/>
-<node CREATED="1391677036996" ID="Freemind_Link_1493609826" MODIFIED="1391677066076" TEXT=" Issue claim-based SAML 1.1 and SAML 2.0 security tokens"/>
-<node CREATED="1391684347110" ID="Freemind_Link_1289308652" MODIFIED="1391684354372" TEXT="Manage Security Token Service">
-<node CREATED="1391684360671" ID="Freemind_Link_1060705757" MODIFIED="1391684361541" TEXT="Configuring STS for Obtaining Tokens with Holder-Of-Key Subject Confirmation"/>
-<node CREATED="1391684367431" ID="Freemind_Link_1808057172" MODIFIED="1391684368132" TEXT="Securing the Security Token Service"/>
-<node CREATED="1391684374391" ID="Freemind_Link_841905456" MODIFIED="1391684375872" TEXT="Configure WSO2 Identity Server to Issue Security Tokens"/>
-<node CREATED="1391684382831" ID="Freemind_Link_680373651" MODIFIED="1391684383501" TEXT="WSO2 Identity Server and Claim Aware Proxy Services with ESB"/>
-<node CREATED="1391684389367" ID="Freemind_Link_1933092074" MODIFIED="1391684391002" TEXT="Extending WSO2 Identity Server to Handle Custom SAML Assertions "/>
-<node CREATED="1391684399382" ID="Freemind_Link_563979080" MODIFIED="1391684400166" TEXT="Accessing Claim Aware Services using STS Secured with Non-repudiation"/>
-<node CREATED="1391684404206" ID="Freemind_Link_785734839" MODIFIED="1391684405024" TEXT="Broker Trust Relationships with WSO2 Identity Server"/>
 </node>
+<node CREATED="1392711974472" ID="Freemind_Link_1537683128" MODIFIED="1392711977728" POSITION="left" TEXT="Single Sign-On">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1392712005271" ID="Freemind_Link_1862397912" MODIFIED="1392712007965" POSITION="right" TEXT="SCIM">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1392712028823" ID="Freemind_Link_462016440" MODIFIED="1392712035818" POSITION="left" TEXT="OAuth">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1391667468584" ID="Freemind_Link_455485563" MODIFIED="1392712166145" POSITION="left" TEXT="Attribute and Claim based access control">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1392712176030" ID="Freemind_Link_356573765" MODIFIED="1392712181507" POSITION="right" TEXT="Claim management">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1392712302204" ID="Freemind_Link_568963512" MODIFIED="1392712314677" POSITION="right" TEXT="Kerberos">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1392712306069" ID="Freemind_Link_1867597087" MODIFIED="1392712313643" POSITION="left" TEXT="Tenant Mode">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1392712892095" ID="Freemind_Link_223420834" MODIFIED="1392712901233" POSITION="right" TEXT="Load/Performance">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1392712939470" ID="Freemind_Link_1587200864" MODIFIED="1392712947783" POSITION="left" TEXT="Integration with different carbon products">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 </map>
--- qa/qa-artifacts/Mind_Maps/IS/OpenID.mm
+++ qa/qa-artifacts/Mind_Maps/IS/OpenID.mm
@@ -30,7 +30,7 @@
 </node>
 <node CREATED="1392204387506" ID="Freemind_Link_597355555" MODIFIED="1392204446250" TEXT="Associate a new user">
 <node CREATED="1392204468505" ID="Freemind_Link_221243476" MODIFIED="1392204503590" TEXT="Login via Username and check for OpenID URL"/>
-<node CREATED="1392204479325" ID="Freemind_Link_480504657" MODIFIED="1392204486437" TEXT="ogin via open ID URL"/>
+<node CREATED="1392204479325" ID="Freemind_Link_480504657" MODIFIED="1392711061138" TEXT="login via open ID URL"/>
 </node>
 </node>
 </node>
@@ -82,7 +82,7 @@
 <node CREATED="1392195838237" ID="Freemind_Link_537624141" MODIFIED="1392195839948" POSITION="left" TEXT="OpenID Directed Identity ">
 <node CREATED="1392195842195" ID="Freemind_Link_4807851" MODIFIED="1392195859780" TEXT="Tenant login"/>
 <node CREATED="1392195863916" ID="Freemind_Link_1422935111" MODIFIED="1392195870509" TEXT="Multiple Userstore">
-<node CREATED="1392196813059" ID="Freemind_Link_1100977578" MODIFIED="1392196955677" TEXT=" Directed Identity Patch and Samples (mail thread)"/>
+<node CREATED="1392196813059" ID="Freemind_Link_1100977578" MODIFIED="1392274397254" TEXT="Ref : Directed Identity Patch and Samples (mail thread)"/>
 </node>
 </node>
 <node CREATED="1392196982724" ID="Freemind_Link_1381129730" MODIFIED="1392197069559" POSITION="left" TEXT="Relying Party (RP) communicatation methods">
@@ -109,15 +109,47 @@
 <node CREATED="1392198742334" ID="Freemind_Link_1131568391" MODIFIED="1392198745265" TEXT="send attributes"/>
 <node CREATED="1392199401161" ID="Freemind_Link_1389625204" MODIFIED="1392199401833" TEXT="authenticte users in phishing resisting manner"/>
 </node>
+<node CREATED="1392268269663" ID="Freemind_Link_162152167" MODIFIED="1392268283374" TEXT="Ref  : ALLMD-28"/>
 </node>
 </node>
 <node CREATED="1392199435673" ID="Freemind_Link_679258994" MODIFIED="1392199439982" POSITION="right" TEXT="Single Sign">
-<node CREATED="1392199446407" ID="Freemind_Link_1574709629" MODIFIED="1392199448017" TEXT="On"/>
+<node CREATED="1392199446407" ID="Freemind_Link_1574709629" MODIFIED="1392199448017" TEXT="On">
+<node CREATED="1392274123935" ID="Freemind_Link_1217980899" MODIFIED="1392274136720" TEXT="With a carbon product E.g., GREG"/>
+</node>
 <node CREATED="1392199450425" ID="Freemind_Link_102069128" MODIFIED="1392199451779" TEXT="Out"/>
 </node>
 <node CREATED="1392199472088" ID="Freemind_Link_1325679017" MODIFIED="1392199474961" POSITION="left" TEXT="Session time out"/>
 <node CREATED="1392203383460" ID="Freemind_Link_510182515" MODIFIED="1392203384547" POSITION="right" TEXT="OpenID Relying party scenarios">
 <node CREATED="1392203473738" ID="Freemind_Link_1811449281" MODIFIED="1392203474459" TEXT="https://docs.google.com/a/wso2.com/spreadsheet/ccc?key=0Ar03qlNzqC4EdDRySlNiRUpyQ3dHSVhoTkVmZ2FZWlE&amp;usp=drive_web#gid=1"/>
+</node>
+<node CREATED="1392268870158" ID="Freemind_Link_1209087763" MODIFIED="1392268903060" POSITION="left" TEXT="For different host names">
+<node CREATED="1392268911750" ID="Freemind_Link_1668810042" MODIFIED="1392268912665" TEXT="http://pavithramadurangi.blogspot.com/2012/03/configurign-openid-based-sso-when-host.html"/>
+</node>
+<node CREATED="1392271715153" ID="Freemind_Link_953041387" MODIFIED="1392271721675" POSITION="right" TEXT="Attributes check">
+<node CREATED="1392271740761" ID="Freemind_Link_1639244352" MODIFIED="1392271741443" TEXT="http://openid.net/specs/openid-authentication-2_0.html"/>
+</node>
+<node CREATED="1392274291342" ID="Freemind_Link_1565880043" MODIFIED="1392274305098" POSITION="right" TEXT="Installing new certificate at the OpenID server">
+<node CREATED="1392274331629" ID="Freemind_Link_1230922011" MODIFIED="1392274334543" TEXT="ALLMD-24"/>
+</node>
+<node CREATED="1392274500556" ID="Freemind_Link_504370965" MODIFIED="1392274501515" POSITION="left" TEXT="OpenID Authentication protocol messages ">
+<node CREATED="1392274522924" ID="Freemind_Link_1899031524" MODIFIED="1392274528523" TEXT="Encoding it in to different language"/>
+<node CREATED="1392274550036" ID="Freemind_Link_1337220446" MODIFIED="1392274552437" TEXT="Ref  : ALLMD-33"/>
+</node>
+<node CREATED="1392274635315" ID="Freemind_Link_673074392" MODIFIED="1392274681004" POSITION="right" TEXT="Install in multiple nodes setup">
+<node CREATED="1392274674674" ID="Freemind_Link_987518345" MODIFIED="1392274675937" TEXT="Client/Browser --&gt; App Server --&gt; LB - OpenID Server ">
+<node CREATED="1392274691593" ID="Freemind_Link_1391985248" MODIFIED="1392274699687" TEXT="Ref  : ALLMD-2"/>
+</node>
+<node CREATED="1392275573564" ID="Freemind_Link_1175515938" MODIFIED="1392275576748" TEXT="Load Test">
+<node CREATED="1392275582177" ID="Freemind_Link_1067438268" MODIFIED="1392275582852" TEXT="Ref  : ALLMD-53"/>
+</node>
+</node>
+<node CREATED="1392274854849" ID="Freemind_Link_1813813492" MODIFIED="1392275217641" POSITION="left" TEXT="Get access tokens ">
+<node CREATED="1392274885705" ID="Freemind_Link_1547200532" MODIFIED="1392274913794" TEXT=" provides user claims">
+<node CREATED="1392274918977" ID="Freemind_Link_1735672453" MODIFIED="1392274919858" TEXT="https://localhost:9443/oauth2endpoints/userinfo?schema=openid"/>
+</node>
+</node>
+<node CREATED="1392711238335" ID="Freemind_Link_50819736" MODIFIED="1392711243424" POSITION="left" TEXT="Custom Login Page">
+<node CREATED="1392711608267" ID="Freemind_Link_1280870029" MODIFIED="1392711609042" TEXT="http://dulanja.blogspot.com/2014/01/wso2-is-samlsso-customizing-login-page.html"/>
 </node>
 </node>
 </map>
