3a4d0bdd1a1babfe21f6cfbf78f17f9f04641eb2
pavithra@wso2.com <pavithra@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2014-02-18 08:09:09 +0000

    Adding openid mind map done by Ushani Balasooriya
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@196950 a5903396-d722-0410-b921-86c7d4935375

---
+++ qa/qa-artifacts/Mind_Maps/IS/OpenID.mm
@@ -0,0 +1,123 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1392183818328" ID="Freemind_Link_1932185721" MODIFIED="1392183844323" TEXT="OpenID">
+<node CREATED="1392183892125" ID="_" MODIFIED="1392184400094" POSITION="right" TEXT="Login to a different App via Open ID E.g., Liferay">
+<node CREATED="1392184375356" ID="Freemind_Link_1998758905" MODIFIED="1392184702980" TEXT="Credential Challenge in Identity provider for the 1st time login">
+<node CREATED="1392184471008" ID="Freemind_Link_1865879239" MODIFIED="1392184474395" TEXT="Select Profile"/>
+</node>
+<node CREATED="1392184674192" ID="Freemind_Link_1660440110" MODIFIED="1392184692747" TEXT="If already signed in">
+<node CREATED="1392184727201" ID="Freemind_Link_1795856506" MODIFIED="1392184730739" TEXT="Select profile"/>
+</node>
+</node>
+<node CREATED="1392184293081" ID="Freemind_Link_1323654222" MODIFIED="1392185260925" POSITION="left" TEXT="XMPP-Based Authentication"/>
+<node CREATED="1392184803863" ID="Freemind_Link_953387160" MODIFIED="1392185360311" POSITION="right" TEXT="Login to Identity Server">
+<node CREATED="1392185350276" ID="Freemind_Link_394166763" MODIFIED="1392186935177" TEXT="Open ID provider. E.g., liferay">
+<node CREATED="1392194282144" ID="Freemind_Link_559825869" MODIFIED="1392194292739" TEXT="Admin mode">
+<node CREATED="1392187365796" ID="Freemind_Link_338716770" MODIFIED="1392187366564" TEXT="http://docs.wso2.org/display/IS460/WSO2+Identity+Server+as+an+OpenID+Provider">
+<node CREATED="1392193701939" ID="Freemind_Link_385289357" LINK="http://docs.wso2.org/display/IS460/WSO2+Identity+Server+as+an+OpenID+Provider" MODIFIED="1392204004385" TEXT="docs.wso2.org/Display/IS460/WSO2+Identity+Server+as+an+OpenID+Provider"/>
+</node>
+<node CREATED="1392186913776" ID="Freemind_Link_1338420500" MODIFIED="1392186914615" TEXT="http://soasecurity.org/2010/07/28/wso2-identity-server-as-openid-provider/"/>
+</node>
+<node CREATED="1392194294536" ID="Freemind_Link_568572061" MODIFIED="1392194308569" TEXT="Tenant mode"/>
+</node>
+<node CREATED="1392186873483" ID="Freemind_Link_940405278" MODIFIED="1392186877490" TEXT="Open ID Consumer">
+<node CREATED="1392186885304" ID="Freemind_Link_928721480" MODIFIED="1392186886088" TEXT="http://soasecurity.org/2011/02/15/wso2identity-server-as-openid-consumer/"/>
+<node CREATED="1392202957174" ID="Freemind_Link_1731040923" MODIFIED="1392202957174" TEXT="">
+<node CREATED="1392199490871" ID="Freemind_Link_1998701423" MODIFIED="1392199494016" TEXT="For admin"/>
+<node CREATED="1392199495760" ID="Freemind_Link_1415093598" MODIFIED="1392199507037" TEXT="For tenant admin"/>
+<node CREATED="1392199508688" ID="Freemind_Link_916155210" MODIFIED="1392199512884" TEXT="For tenant users"/>
+<node CREATED="1392199516240" ID="Freemind_Link_851853283" MODIFIED="1392199520591" TEXT="For self registered users"/>
+</node>
+<node CREATED="1392204387506" ID="Freemind_Link_597355555" MODIFIED="1392204446250" TEXT="Associate a new user">
+<node CREATED="1392204468505" ID="Freemind_Link_221243476" MODIFIED="1392204503590" TEXT="Login via Username and check for OpenID URL"/>
+<node CREATED="1392204479325" ID="Freemind_Link_480504657" MODIFIED="1392204486437" TEXT="ogin via open ID URL"/>
+</node>
+</node>
+</node>
+<node CREATED="1392185753305" ID="Freemind_Link_1232485775" MODIFIED="1392204600484" POSITION="left" TEXT="Integrate with OpenID/social networks">
+<node CREATED="1392185761841" ID="Freemind_Link_1078259035" MODIFIED="1392197390763" TEXT="Ref : Integrate with OpenID/social networks (support) - @in username"/>
+</node>
+<node CREATED="1392186108270" ID="Freemind_Link_1441301682" MODIFIED="1392193526592" POSITION="right" TEXT="Login to a client web app using OpenID">
+<node CREATED="1392187283405" ID="Freemind_Link_963515778" MODIFIED="1392187284902" TEXT="http://pavithramadurangi.blogspot.com/2013/09/openid-with-wso2-is-450.html"/>
+</node>
+<node CREATED="1392188514380" ID="Freemind_Link_1844091731" MODIFIED="1392188515734" POSITION="right" TEXT=" Custom Authenticator for carbon version 4.2.0">
+<node CREATED="1392188524884" ID="Freemind_Link_454023355" MODIFIED="1392188525677" TEXT="http://venurakahawala.blogspot.com/2013/11/creating-custom-authenticator-for.html"/>
+</node>
+<node CREATED="1392188716747" ID="Freemind_Link_1972279693" MODIFIED="1392188763398" POSITION="left" TEXT="Code level changes can be done">
+<node CREATED="1392188783114" ID="Freemind_Link_984387975" MODIFIED="1392188836791" TEXT="Forget password"/>
+<node CREATED="1392188841658" ID="Freemind_Link_667028068" MODIFIED="1392197382243" TEXT="Ref: WSo2 identity Server - OpenID - Clarifications (support)"/>
+</node>
+<node CREATED="1392188906393" ID="Freemind_Link_380179096" MODIFIED="1392192230666" POSITION="right" TEXT="Login to an app by a user who is restricted by a xacml"/>
+<node CREATED="1392194449471" ID="Freemind_Link_1577831918" MODIFIED="1392194453129" POSITION="left" TEXT="Remember Me">
+<node CREATED="1392199553096" ID="Freemind_Link_1267691087" MODIFIED="1392199598895" TEXT="User name OpenIDUserPattern=enabled">
+<node CREATED="1392199490871" ID="Freemind_Link_1458733171" MODIFIED="1392199494016" TEXT="For admin"/>
+<node CREATED="1392199495760" ID="Freemind_Link_417300452" MODIFIED="1392199507037" TEXT="For tenant admin"/>
+<node CREATED="1392199508688" ID="Freemind_Link_1654037044" MODIFIED="1392199512884" TEXT="For tenant users"/>
+<node CREATED="1392199516240" ID="Freemind_Link_744489533" MODIFIED="1392199520591" TEXT="For self registered users"/>
+</node>
+<node CREATED="1392199564551" ID="Freemind_Link_1165388416" MODIFIED="1392199610897" TEXT="URL OpenIDUserPattern=disabled">
+<node CREATED="1392199490871" ID="Freemind_Link_1395628836" MODIFIED="1392199494016" TEXT="For admin"/>
+<node CREATED="1392199495760" ID="Freemind_Link_1419740267" MODIFIED="1392199507037" TEXT="For tenant admin"/>
+<node CREATED="1392199508688" ID="Freemind_Link_1906639093" MODIFIED="1392199512884" TEXT="For tenant users"/>
+<node CREATED="1392199516240" ID="Freemind_Link_1461293337" MODIFIED="1392199520591" TEXT="For self registered users"/>
+</node>
+</node>
+<node CREATED="1392194496127" ID="Freemind_Link_1091198253" MODIFIED="1392199680989" POSITION="right" TEXT="Approve">
+<node CREATED="1392199668463" ID="Freemind_Link_1853271524" MODIFIED="1392199671664" TEXT="Approve always">
+<node CREATED="1392199490871" ID="Freemind_Link_26213257" MODIFIED="1392199494016" TEXT="For admin"/>
+<node CREATED="1392199495760" ID="Freemind_Link_713046087" MODIFIED="1392199507037" TEXT="For tenant admin"/>
+<node CREATED="1392199508688" ID="Freemind_Link_1157663005" MODIFIED="1392199512884" TEXT="For tenant users"/>
+<node CREATED="1392199516240" ID="Freemind_Link_1605202270" MODIFIED="1392199520591" TEXT="For self registered users"/>
+</node>
+<node CREATED="1392199689199" ID="Freemind_Link_297876512" MODIFIED="1392199692498" TEXT="No Approval">
+<node CREATED="1392194417831" ID="Freemind_Link_348543179" MODIFIED="1392194419192" TEXT="Check OpenIDSkipUserConsent property">
+<node CREATED="1392199490871" ID="Freemind_Link_1459896058" MODIFIED="1392199494016" TEXT="For admin"/>
+<node CREATED="1392199495760" ID="Freemind_Link_534856726" MODIFIED="1392199507037" TEXT="For tenant admin"/>
+<node CREATED="1392199508688" ID="Freemind_Link_1377604684" MODIFIED="1392199512884" TEXT="For tenant users"/>
+<node CREATED="1392199516240" ID="Freemind_Link_1975734782" MODIFIED="1392199520591" TEXT="For self registered users"/>
+<node CREATED="1392186168262" ID="Freemind_Link_1419785257" MODIFIED="1392186169376" TEXT="http://sureshatt.blogspot.com/2012/03/openid-based-sso-with-wso2-identity.html"/>
+</node>
+</node>
+</node>
+<node CREATED="1392195838237" ID="Freemind_Link_537624141" MODIFIED="1392195839948" POSITION="left" TEXT="OpenID Directed Identity ">
+<node CREATED="1392195842195" ID="Freemind_Link_4807851" MODIFIED="1392195859780" TEXT="Tenant login"/>
+<node CREATED="1392195863916" ID="Freemind_Link_1422935111" MODIFIED="1392195870509" TEXT="Multiple Userstore">
+<node CREATED="1392196813059" ID="Freemind_Link_1100977578" MODIFIED="1392196955677" TEXT=" Directed Identity Patch and Samples (mail thread)"/>
+</node>
+</node>
+<node CREATED="1392196982724" ID="Freemind_Link_1381129730" MODIFIED="1392197069559" POSITION="left" TEXT="Relying Party (RP) communicatation methods">
+<node CREATED="1392196445000" ID="Freemind_Link_708634639" MODIFIED="1392196457993" TEXT="Dumb Mode (Statefull)">
+<node CREATED="1392198234348" ID="Freemind_Link_1465139761" MODIFIED="1392198236827" TEXT="Enable">
+<node CREATED="1392196460392" ID="Freemind_Link_1698932756" MODIFIED="1392196469585" TEXT="http://sureshatt.blogspot.com/2011/05/openid-dumb-mode-consumer-with.html"/>
+</node>
+<node CREATED="1392198240637" ID="Freemind_Link_55267104" MODIFIED="1392198394202" TEXT=" (DisableOpenIDDumbMode) Disable"/>
+<node CREATED="1392198494680" ID="Freemind_Link_1174741076" MODIFIED="1392198513815" TEXT="RP (Relying Party)">
+<node CREATED="1392199349159" ID="Freemind_Link_337035092" MODIFIED="1392199361627" TEXT="Fetch attributes"/>
+<node CREATED="1392199365264" ID="Freemind_Link_404536722" MODIFIED="1392199369115" TEXT="Store attributes"/>
+</node>
+</node>
+<node CREATED="1392196988844" ID="Freemind_Link_1147470967" MODIFIED="1392197000733" TEXT="Smart Mode (Stateless)">
+<node CREATED="1392198558400" ID="Freemind_Link_406667104" MODIFIED="1392198566129" TEXT="OpenID Provider (OP)">
+<node CREATED="1392198729350" ID="Freemind_Link_230823417" MODIFIED="1392198740542" TEXT="Store attributes">
+<node CREATED="1392186599459" ID="Freemind_Link_692432062" MODIFIED="1392186600976" TEXT="Attribute Exchange with Google">
+<node CREATED="1392204963785" ID="Freemind_Link_1240109905" MODIFIED="1392204997897" TEXT=" With approval">
+<node CREATED="1392186612194" ID="Freemind_Link_848501863" MODIFIED="1392186612860" TEXT="http://sureshatt.blogspot.de/2011/05/openid-consumer-for-attribute-exchange.html"/>
+</node>
+<node CREATED="1392204978126" ID="Freemind_Link_232404086" MODIFIED="1392205015175" TEXT="(Check OpenIDSkipUserConsent=true property) No Approval"/>
+</node>
+</node>
+<node CREATED="1392198742334" ID="Freemind_Link_1131568391" MODIFIED="1392198745265" TEXT="send attributes"/>
+<node CREATED="1392199401161" ID="Freemind_Link_1389625204" MODIFIED="1392199401833" TEXT="authenticte users in phishing resisting manner"/>
+</node>
+</node>
+</node>
+<node CREATED="1392199435673" ID="Freemind_Link_679258994" MODIFIED="1392199439982" POSITION="right" TEXT="Single Sign">
+<node CREATED="1392199446407" ID="Freemind_Link_1574709629" MODIFIED="1392199448017" TEXT="On"/>
+<node CREATED="1392199450425" ID="Freemind_Link_102069128" MODIFIED="1392199451779" TEXT="Out"/>
+</node>
+<node CREATED="1392199472088" ID="Freemind_Link_1325679017" MODIFIED="1392199474961" POSITION="left" TEXT="Session time out"/>
+<node CREATED="1392203383460" ID="Freemind_Link_510182515" MODIFIED="1392203384547" POSITION="right" TEXT="OpenID Relying party scenarios">
+<node CREATED="1392203473738" ID="Freemind_Link_1811449281" MODIFIED="1392203474459" TEXT="https://docs.google.com/a/wso2.com/spreadsheet/ccc?key=0Ar03qlNzqC4EdDRySlNiRUpyQ3dHSVhoTkVmZ2FZWlE&amp;usp=drive_web#gid=1"/>
+</node>
+</node>
+</map>
