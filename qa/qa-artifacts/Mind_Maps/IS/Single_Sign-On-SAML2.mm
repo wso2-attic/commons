a01fa28dd40df09dc6254e0e54e071f66884fa36
pavithra@wso2.com <pavithra@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2014-02-19 09:19:50 +0000

    Mindmaps for SSO - done by Ushani Balasooriya
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/commons@197023 a5903396-d722-0410-b921-86c7d4935375

---
+++ qa/qa-artifacts/Mind_Maps/IS/Single_Sign-On-SAML2.mm
@@ -0,0 +1,107 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1392192698298" ID="Freemind_Link_923384270" MODIFIED="1392197554240" TEXT="SAML2 based Single Sign-On">
+<node CREATED="1392192957728" ID="_" MODIFIED="1392193696694" POSITION="right" TEXT="As a consumer">
+<node CREATED="1392206969484" ID="Freemind_Link_1953130601" MODIFIED="1392206977640" TEXT="With Shibboleth SAML2 IDP"/>
+</node>
+<node CREATED="1392193707396" ID="Freemind_Link_1582613516" MODIFIED="1392210998724" POSITION="left" TEXT="As an identity provider">
+<node CREATED="1392196470118" ID="Freemind_Link_1351194519" MODIFIED="1392291551619" TEXT="SAML2 Web Browser Single Sign-On profile">
+<node CREATED="1392211259190" ID="Freemind_Link_1906887253" MODIFIED="1392211282476" TEXT="Single Identity providers"/>
+<node CREATED="1392211285357" ID="Freemind_Link_847720508" MODIFIED="1392211294166" TEXT="Multiple Identity providers"/>
+<node CREATED="1392713456807" ID="Freemind_Link_1331057784" MODIFIED="1392713459068" TEXT="Login">
+<node CREATED="1392199490871" ID="Freemind_Link_1998701423" MODIFIED="1392199494016" TEXT="For admin"/>
+<node CREATED="1392199495760" ID="Freemind_Link_1415093598" MODIFIED="1392199507037" TEXT="For tenant admin"/>
+<node CREATED="1392199508688" ID="Freemind_Link_916155210" MODIFIED="1392199512884" TEXT="For tenant users"/>
+<node CREATED="1392199516240" ID="Freemind_Link_851853283" MODIFIED="1392199520591" TEXT="For self registered users"/>
+<node CREATED="1392716446739" ID="Freemind_Link_173046030" MODIFIED="1392716453590" TEXT="log in via SAML with email user name (@)">
+<node CREATED="1392716475152" ID="Freemind_Link_1871208488" MODIFIED="1392716475946" TEXT="https://wso2.org/jira/browse/CARBON-13048"/>
+</node>
+</node>
+</node>
+<node CREATED="1392197573249" ID="Freemind_Link_1981814659" MODIFIED="1392197577874" TEXT="Single Logout Profile"/>
+<node CREATED="1392291766093" ID="Freemind_Link_1058930422" MODIFIED="1392291770231" TEXT="Request Attributes">
+<node CREATED="1392291794516" ID="Freemind_Link_1945546743" MODIFIED="1392291795983" TEXT="SAMLAttributeConsumingServiceIndex">
+<node CREATED="1392615887924" ID="Freemind_Link_1817855274" MODIFIED="1392615888768" TEXT="http://sureshatt.blogspot.com/search/label/saml2"/>
+</node>
+</node>
+<node CREATED="1392291808492" ID="Freemind_Link_969311813" MODIFIED="1392291834815" TEXT="Change IDP URL and Discover the new IS server"/>
+<node CREATED="1392291850100" ID="Freemind_Link_893124753" MODIFIED="1392626019637" TEXT="Session timeout">
+<node CREATED="1392626001406" ID="Freemind_Link_1214807113" MODIFIED="1392626015428" TEXT="Browser session life time"/>
+<node CREATED="1392626034328" ID="Freemind_Link_1434945189" MODIFIED="1392626036056" TEXT="skip authentication prompt for SAML SSO if OpenID authenticated browser session available "/>
+<node CREATED="1392626057349" ID="Freemind_Link_337410049" MODIFIED="1392626074141" TEXT="skip authentication prompt for OpenID if SAML SSO authenticated browser session availble "/>
+</node>
+</node>
+<node CREATED="1392615539318" ID="Freemind_Link_351673140" MODIFIED="1392615541092" POSITION="left" TEXT="Enabling SSO for WSO2 Servers"/>
+<node CREATED="1392618214615" ID="Freemind_Link_54454492" MODIFIED="1392618215506" POSITION="left" TEXT="Read A SAML 2.0 Response With OpenSAML">
+<node CREATED="1392618238805" ID="Freemind_Link_1579181500" MODIFIED="1392618239754" TEXT="http://sureshatt.blogspot.com/2012/11/how-to-read-saml-20-response-with.html"/>
+</node>
+<node CREATED="1392626102189" ID="Freemind_Link_745444878" MODIFIED="1392626104767" POSITION="right" TEXT="Log out"/>
+<node CREATED="1392626141988" ID="Freemind_Link_1424385772" MODIFIED="1392626166620" POSITION="left" TEXT="Exchange a SAML token issued to an OAuth access token from SaleseForce "/>
+<node CREATED="1392627872877" ID="Freemind_Link_1365783983" MODIFIED="1392627884158" POSITION="right" TEXT="Integrating SAML2 with Liferay using JOSSO Agent plug-in"/>
+<node CREATED="1392628317025" ID="Freemind_Link_1181063608" MODIFIED="1392628351659" POSITION="left" TEXT="Enable HTTPs in tomcat  ">
+<node CREATED="1392628658589" ID="Freemind_Link_1996915704" MODIFIED="1392629824355" TEXT=" use https URL in ssoServiceEpr via mgt console"/>
+</node>
+<node CREATED="1392629263792" ID="Freemind_Link_325179632" MODIFIED="1392629347791" POSITION="right" TEXT="Check for attributes when creating a new service provider"/>
+<node CREATED="1392635011762" ID="Freemind_Link_1341201093" MODIFIED="1392635018330" POSITION="left" TEXT="Retrieve customer attributes"/>
+<node CREATED="1392635049129" ID="Freemind_Link_1657192867" MODIFIED="1392635138314" POSITION="right" TEXT="Get customer attributes">
+<node CREATED="1392635119121" ID="Freemind_Link_317111219" MODIFIED="1392635126497" TEXT="Get Via secondary user store">
+<node CREATED="1392615987259" ID="Freemind_Link_14123986" MODIFIED="1392618683494" TEXT="Getting Claims">
+<node CREATED="1392616007443" ID="Freemind_Link_817230014" MODIFIED="1392618699459" TEXT="User Claims ">
+<node CREATED="1392618692450" ID="Freemind_Link_446230598" MODIFIED="1392618703221" TEXT="http://sureshatt.blogspot.com/2012/11/getting-user-claims-over-saml-sso-token.html"/>
+</node>
+<node CREATED="1392618641299" ID="Freemind_Link_109204152" MODIFIED="1392618649055" TEXT="Custom claims">
+<node CREATED="1392618656761" ID="Freemind_Link_1981493369" MODIFIED="1392618657558" TEXT="http://sureshatt.blogspot.com/2014/01/getting-custom-claims-over-saml-sso.html"/>
+</node>
+</node>
+</node>
+<node CREATED="1392635149353" ID="Freemind_Link_346084647" MODIFIED="1392635155241" TEXT="Get via primary user store">
+<node CREATED="1392615987259" ID="Freemind_Link_1914552111" MODIFIED="1392618683494" TEXT="Getting Claims">
+<node CREATED="1392616007443" ID="Freemind_Link_1713105389" MODIFIED="1392618699459" TEXT="User Claims ">
+<node CREATED="1392618692450" ID="Freemind_Link_629527018" MODIFIED="1392618703221" TEXT="http://sureshatt.blogspot.com/2012/11/getting-user-claims-over-saml-sso-token.html"/>
+</node>
+<node CREATED="1392618641299" ID="Freemind_Link_671957141" MODIFIED="1392618649055" TEXT="Custom claims">
+<node CREATED="1392618656761" ID="Freemind_Link_993076390" MODIFIED="1392618657558" TEXT="http://sureshatt.blogspot.com/2014/01/getting-custom-claims-over-saml-sso.html"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1392635315111" ID="Freemind_Link_1802289546" MODIFIED="1392635321872" POSITION="right" TEXT="Install in carbon products">
+<node CREATED="1392290163366" ID="Freemind_Link_152079926" MODIFIED="1392290268955" TEXT="Install in a carbon server via P2 "/>
+<node CREATED="1392635332665" ID="Freemind_Link_1139302350" MODIFIED="1392635352432" TEXT="In different carbon products via configuration">
+<node CREATED="1392635382398" ID="Freemind_Link_1674829297" MODIFIED="1392635385279" TEXT=" SSO server on multiple nodes"/>
+<node CREATED="1392635388862" ID="Freemind_Link_1038419000" MODIFIED="1392635400001" TEXT="configure in one product"/>
+</node>
+</node>
+<node CREATED="1392703275392" ID="Freemind_Link_1643246825" MODIFIED="1392703288974" POSITION="left" TEXT="Change attributes in conf files and check the behaviour"/>
+<node CREATED="1392705479525" ID="Freemind_Link_631478511" MODIFIED="1392705483270" POSITION="right" TEXT="Obtain OAuth2 token">
+<node CREATED="1392705493677" ID="Freemind_Link_1747305220" MODIFIED="1392717437169" TEXT="SAML Assertion- Access token">
+<node CREATED="1392705518748" ID="Freemind_Link_1263216687" MODIFIED="1392705519438" TEXT="http://tools.ietf.org/html/draft-ietf-oauth-saml2-bearer-03"/>
+<node CREATED="1392716513992" ID="Freemind_Link_693753545" MODIFIED="1392716514577" TEXT="http://docs.wso2.org/display/IS460/SAML2+Bearer+Assertion+Profile+for+OAuth+2.0"/>
+</node>
+<node CREATED="1392717418711" ID="Freemind_Link_1072297901" MODIFIED="1392717443994" TEXT="Refresh Token"/>
+</node>
+<node CREATED="1392713485554" ID="Freemind_Link_1065415849" MODIFIED="1392713491099" POSITION="left" TEXT="Load/Performance Test"/>
+<node CREATED="1392713494434" ID="Freemind_Link_9118778" MODIFIED="1392713523469" POSITION="right" TEXT="Custom Login Page">
+<node CREATED="1392713526265" ID="Freemind_Link_530906580" MODIFIED="1392713526883" TEXT="http://dulanja.blogspot.com/2014/01/wso2-is-samlsso-customizing-login-page.html"/>
+</node>
+<node CREATED="1392718104265" ID="Freemind_Link_74178418" MODIFIED="1392718151570" POSITION="left" TEXT="Different User Stores">
+<node CREATED="1391667137928" ID="Freemind_Link_598419819" MODIFIED="1391667146067" TEXT="External LDAP"/>
+<node CREATED="1391667155833" ID="Freemind_Link_120010034" MODIFIED="1391667165713" TEXT="Micr AD"/>
+<node CREATED="1391667170631" ID="Freemind_Link_1070697970" MODIFIED="1391667174204" TEXT="JDBC connection"/>
+<node CREATED="1391667130832" ID="Freemind_Link_1082543796" MODIFIED="1391667151752" TEXT="Built In LDAP"/>
+<node CREATED="1392722997991" ID="Freemind_Link_686393878" MODIFIED="1392723012304" TEXT="Secondary User store"/>
+</node>
+<node CREATED="1392718654516" ID="Freemind_Link_1758144891" MODIFIED="1392718667165" POSITION="right" TEXT="Authorization delegation">
+<node CREATED="1392718669523" ID="Freemind_Link_26034660" MODIFIED="1392718672015" TEXT="Over SOAP">
+<node CREATED="1392718713899" ID="Freemind_Link_1939470630" MODIFIED="1392718726684" TEXT="CNECD-55"/>
+</node>
+</node>
+<node CREATED="1392722300118" ID="Freemind_Link_1911094250" MODIFIED="1392722437069" POSITION="left" TEXT="SAML Federated SSO feature with other IDPs">
+<node CREATED="1392722532939" ID="Freemind_Link_483274550" MODIFIED="1392722533579" TEXT="Feature #592"/>
+<node CREATED="1392722762505" ID="Freemind_Link_252211267" MODIFIED="1392722868042" TEXT="ALCTD-2"/>
+</node>
+<node CREATED="1392722492212" ID="Freemind_Link_837806959" MODIFIED="1392722493677" POSITION="right" TEXT="Encrypted Assertions in SAML2 SSO">
+<node CREATED="1392722501147" ID="Freemind_Link_1088327112" MODIFIED="1392722501805" TEXT="Feature #2246"/>
+</node>
+</node>
+</map>
